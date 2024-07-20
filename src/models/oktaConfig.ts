3c314c640fdfe75e43508ab4e42b9c661d997a59
Egemen Sener <egemensener98@gmail.com>
2024-07-20 12:15:33 +0300

    okta config added

---
+++ src/models/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oaigke4fdAnyySbg5d7',
+    issuer: 'https://dev-14905665.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
