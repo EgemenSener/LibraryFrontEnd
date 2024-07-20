11f9bdb2a406009538a1c19852112f1094af8f9a
Egemen Sener <egemensener98@gmail.com>
2024-07-20 19:28:24 +0300

    bookCheckout

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -22,7 +22,11 @@ export const BookCheckoutPage = () => {
 
     // Loans Count State
     const [currentLoansCount, setCurrentLoansCount] = useState(0);
-    const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] = useState(false);
+    const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] = useState(true);
+
+    // Is Book Check Out?
+    const [isCheckedOut, setIsCheckedOut] = useState(false);
+    const [isLoadingBookCheckedOut, setIsLoadingBookCheckedOut] = useState(true);
 
     const bookId = (window.location.pathname).split('/')[2];
 
@@ -127,9 +131,38 @@ export const BookCheckoutPage = () => {
             setHttpError(error.message);
         })
 
-    },[authState])
+    },[authState]);
 
-    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount) {
+    useEffect(() => {
+        const fetchUserCheckedOutBook = async () => {
+            if(authState && authState.isAuthenticated) {
+                const url = `http://localhost:8080/api/books/secure/ischeckedout/byuser?bookId=${bookId}`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const bookCheckedOut = await fetch(url, requestOptions);
+
+                if(!bookCheckedOut.ok) {
+                    throw new Error('Something went wrong!');
+                }
+
+                const bookCheckedOutResponseJson = await bookCheckedOut.json();
+
+                setIsCheckedOut(bookCheckedOutResponseJson);
+            }
+            setIsLoadingBookCheckedOut(false);
+        }
+        fetchUserCheckedOutBook().catch((error: any) => {
+            setIsLoadingBookCheckedOut(false);
+            setHttpError(error.message);
+        })
+    }, [authState]);
+
+    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingBookCheckedOut) {
         return (
             <SpinnerLoading />
         )
