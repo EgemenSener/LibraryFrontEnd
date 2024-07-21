3a770ff915816323e26a7e6d7f2c8fe39b1809a7
Egemen Sener <egemensener98@gmail.com>
2024-07-21 21:30:12 +0300

    review list page completed!

--- src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
+++ src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react"
 import ReviewModel from "../../../models/ReviewModel"
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Review } from "../../Utils/Review";
+import { Pagination } from "../../Utils/Pagination";
 
 export const ReviewListPage = () => {
     const [reviews, setReviews] = useState<ReviewModel[]>([]);
@@ -16,7 +19,7 @@ export const ReviewListPage = () => {
 
     useEffect(() => {
         const fetchBookReviews = async () => {
-            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}&page=${currentPage}&size=${reviewsPerPage}`;
+            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}&page=${currentPage - 1}&size=${reviewsPerPage}`;
 
             const responseReviews = await fetch(reviewUrl);
 
@@ -52,4 +55,40 @@ export const ReviewListPage = () => {
             setHttpError(error.message);
         })
     }, [currentPage]);
+
+    if(isLoading) {
+        return (<SpinnerLoading />);
+    }
+
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    const indexOfLastReview: number = currentPage * reviewsPerPage;
+    const indexOfFirstReview: number = indexOfLastReview - reviewsPerPage;
+
+    let lastItem = reviewsPerPage * currentPage <= totalAmountOfReviews ? reviewsPerPage * currentPage : totalAmountOfReviews;
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    return (
+        <div className="container mt-5">
+            <div>
+                <h3>Comments: ({reviews.length})</h3>
+            </div>
+            <p>
+                {indexOfFirstReview + 1} to {lastItem} of {totalAmountOfReviews} items:
+            </p>
+            <div className="row">
+                {reviews.map(review => (
+                    <Review review={review} key={review.id} />
+                ))}
+            </div>
+            {totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>}
+        </div>
+    )
 }
